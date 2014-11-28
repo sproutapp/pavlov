61b5de0465b00d679b2bd332b5cd135094b7f933
Bruno Abrantes <bruno@brunoabrantes.com>
2014-11-28 16:42:17 +0100

    Implements .describe

--- lib/case.ex
+++ lib/case.ex
@@ -17,11 +17,11 @@ defmodule Pavlov.Case do
 
     quote do
       use ExUnit.Case, async: unquote(async)
+
+      @stack []
       import Pavlov.Case
     end
   end
-
-  import ExUnit.Case
 
   @doc """
   The cornerstone BDD macro, "it" allows your test to be defined
@@ -32,15 +32,55 @@ defmodule Pavlov.Case do
       assert true == true
     end
   """
-  defmacro it(message, var \\ quote(do: _), contents) do
-    contents = Macro.escape(contents, unquote: true)
+  defmacro it(description, var \\ quote(do: _), contents) do
+    quote do
+      defit Enum.join(@stack, "") <> unquote(description), unquote(var) do
+        unquote(contents)
+      end
+    end
+  end
+
+  @doc """
+  You can nest your tests under a descriptive name.
+  Tests can be infinitely nested.
+  """
+  defmacro describe(description, _ \\ quote(do: _), contents) do
+    quote do
+      @stack Enum.concat(@stack, [unquote(description) <> ", "])
+
+      unquote(contents)
+
+      # Cleans context stack
+      if Enum.count(@stack) > 0 do
+        @stack Enum.take(@stack, Enum.count(@stack) - 1)
+      end
+    end
+  end
+
+  @doc false
+  defmacro defit(message, var \\ quote(do: _), contents) do
+    contents =
+      case contents do
+        [do: _] ->
+          quote do
+            unquote(contents)
+            :ok
+          end
+          _ ->
+            quote do
+              try(unquote(contents))
+              :ok
+            end
+          end
+
     var      = Macro.escape(var)
+    contents = Macro.escape(contents, unquote: true)
 
     quote bind_quoted: binding do
-      test = :"It #{message}"
-      ExUnit.Case.__on_definition__(__ENV__, test)
+      message = :"#{message}"
+      ExUnit.Case.__on_definition__(__ENV__, message)
 
-      def unquote(test)(unquote(var)), do: unquote(contents)
+      def unquote(message)(unquote(var)), do: unquote(contents)
     end
   end
 end
--- test/pavlov_test.exs
+++ test/pavlov_test.exs
@@ -1,7 +1,25 @@
 defmodule PavlovTest do
-  use Pavlov.Case
+  use Pavlov.Case, async: true
 
   it "is the truth" do
     assert 1 + 1 == 2
+  end
+
+  describe "Context" do
+    it "allows nesting" do
+      assert 1 + 1 == 2
+    end
+
+    describe "With multiple levels" do
+      it "also allows nesting" do
+        assert 1 + 1 == 2
+      end
+    end
+  end
+
+  describe "Another Context" do
+    it "allows nesting" do
+      assert 1 + 1 == 2
+    end
   end
 end
