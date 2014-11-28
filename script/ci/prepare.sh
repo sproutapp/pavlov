83ccb2a80188f7568c48a2598f10d064c4628fc7
Bruno Abrantes <bruno@brunoabrantes.com>
2014-11-28 16:55:39 +0100

    Adds Circle CI support

---
+++ script/ci/prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+sudo dpkg -i erlang-solutions_1.0_all.deb
+sudo apt-get update
+sudo apt-get install elixir
+
+# Fetch and compile dependencies and application code (and include testing tools)
+export MIX_ENV="test"
+cd $HOME/$CIRCLE_PROJECT_REPONAME
+mix do deps.get, deps.compile, compile
